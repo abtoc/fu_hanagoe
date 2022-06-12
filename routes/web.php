a70a4e6ac958bb941e665142649512f20b62b7dc
Toshio Abe <abtoc88@gmail.com>
2022-06-12 17:40:53 +0900

    ユーザ認証追加

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify' => true]);
+
+Route::group(['middleware' => ['auth', 'verified']], function(){
+    Route::get('/home', 'HomeController@index')->name('home');
+});
