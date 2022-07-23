b33d7507f0fa8db61f73df40ffc693e9f62e1d6b
Toshio Abe <abtoc88@gmail.com>
2022-07-24 03:49:33 +0900

    見直し

--- resources/views/channel/show.blade.php
+++ resources/views/channel/show.blade.php
@@ -93,11 +93,11 @@
             array_push($views, $transaction->view_count);
             if(($index !== $last)  or ($transaction->view_count_daily > 0)){
                 array_push($views_daily, $transaction->view_count_daily);
-            }
-            if($transaction->video_count === 0){
-                array_push($views_videos, 0);
-            } else {
-                array_push($views_videos, round($transaction->view_count / $transaction->video_count));
+                if($transaction->video_count === 0){
+                    array_push($views_videos, 0);
+                } else {
+                    array_push($views_videos, round($transaction->view_count / $transaction->video_count));
+                }
             }
         }
     @endphp   
